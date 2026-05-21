e6fd17eb38ed9267b688158422527871a7279ad5
RGPin <157803400+RGPin@users.noreply.github.com>
2026-05-21 15:15:58 +0800

    set up ejs

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ const path = require("node:path");
 
 const app = express();
 
-//set up ejs or nah?
-
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", (req, res) => res.send("Haru warudo"));
+app.get("/", (req, res) => res.render("index"));
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, (error) => {
